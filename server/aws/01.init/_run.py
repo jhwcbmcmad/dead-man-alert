093651eff600021a0f27a9a7cc567c8084be276c
pzvefjjhj <rnyqqdwyv@qppoyorb.com>
2019-10-13 21:58:27 +0800

    add rsa key in init

--- server/aws/01.init/_run.py
+++ server/aws/01.init/_run.py
@@ -1,5 +1,9 @@
 import boto3
 import futsu.json
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+import io
 
 def create_kms_key(env_data):
     key_alias_name = 'alias/dead-man-alert_{}'.format(env_data['ENV_KEY'])
@@ -65,11 +69,60 @@ def create_s3_bucket(env_data):
             }
         )
 
+def create_server_main_private_key(env_data):
+    ENV_KEY = env_data['ENV_KEY']
+    AWS_REGION = env_data['AWS_REGION']
+
+    kms_key_alias_name = 'alias/dead-man-alert_{}'.format(ENV_KEY)
+    kms_client = boto3.client('kms',region_name=AWS_REGION)
+    result = kms_client.list_aliases()
+    result = result['Aliases']
+    result = filter(lambda i:i['AliasName']==kms_key_alias_name, result)
+    result = list(result)
+    assert(len(result)==1)
+    kms_key_id = result[0]['TargetKeyId']
+
+    bucket_name = 'dead-man-alert-{ENV_KEY}-perm'.format(ENV_KEY=ENV_KEY)
+    object_key = 'server/private_key.enc'
+    s3_client = boto3.client('s3')
+    result = s3_client.list_objects_v2(
+        Bucket=bucket_name,
+        Prefix=object_key,
+    )
+    result = result['Contents'] if 'Contents' in result else []
+    result = filter(lambda i:i['Key']==object_key, result)
+    result = list(result)
+    if len(result)==1: return
+    assert(len(result)==0)
+    
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+        backend=default_backend(),
+    )
+    private_key_bytes = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+    result = kms_client.encrypt(
+        KeyId=kms_key_id,
+        Plaintext=private_key_bytes
+    )
+    private_key_bytes_enc = result['CiphertextBlob']
+    
+    s3_client.upload_fileobj(
+        io.BytesIO(private_key_bytes_enc),
+        Bucket=bucket_name,
+        Key=object_key
+    )
+
 def main(env_json_filename):
     env_data = futsu.json.file_to_data(env_json_filename)
 
     create_kms_key(env_data)
     create_s3_bucket(env_data)
+    create_server_main_private_key(env_data)
 
 if __name__ == "__main__":
     import argparse
